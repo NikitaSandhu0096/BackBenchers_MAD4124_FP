2d5326a5ff2176778121b3208edc4a5f2cda46f8
kamalpreet kaur <kamal777682>
2020-07-04 19:14:46 -0400

    Notes adapter added

--- app/src/main/java/com/example/backbenchers_mad4124_fp/adapters/SubjectsAdapter.java
+++ app/src/main/java/com/example/backbenchers_mad4124_fp/adapters/SubjectsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.backbenchers_mad4124_fp.adapters;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +10,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.backbenchers_mad4124_fp.MainActivity;
 import com.example.backbenchers_mad4124_fp.R;
 import com.example.backbenchers_mad4124_fp.models.Subject;
+import com.example.backbenchers_mad4124_fp.NotesActivity;
 
 import java.util.ArrayList;
 
@@ -30,13 +33,16 @@ public class SubjectsAdapter extends RecyclerView.Adapter<SubjectsAdapter.Subjec
 
     @Override
     public void onBindViewHolder(@NonNull SubjectViewHolder holder, final int position) {
-        Subject subject = this.subjectArrayList.get(position);
+        final Subject subject = this.subjectArrayList.get(position);
         holder.txtSubject.setText(subject.getSubjectName());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Subject subject1 = subjectArrayList.get(position);
-                Toast.makeText(v.getContext(),"S : ", Toast.LENGTH_SHORT).show();
+               Intent sIntent = new Intent(v.getContext(), NotesActivity.class);
+               // sIntent.putExtra("Subject",subject1.getSubjectName());
+              //startActivity(sIntent);
+            Toast.makeText(v.getContext(),"S : ", Toast.LENGTH_SHORT).show();
             }
         });
     }
