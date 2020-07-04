8883107cadc183306625fe6b9b72c3ddf40f62bd
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-07-03 22:18:09 -0400

    hide fab

--- app/src/main/java/com/example/backbenchers_mad4124_fp/MainActivity.java
+++ app/src/main/java/com/example/backbenchers_mad4124_fp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.backbenchers_mad4124_fp;
 
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -31,43 +32,64 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         final TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        final FloatingActionButton fab = findViewById(R.id.fab);
 
         TabLayout.Tab tab = tabs.getTabAt(0);
         tab.setText("Subjects");
+        tab.setIcon(R.drawable.ic_action_name_subject);
 
         TabLayout.Tab tab1 = tabs.getTabAt(1);
         tab1.setText("Maps");
+        tab1.setIcon(R.drawable.ic_action_name_map);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (tabs.getSelectedTabPosition() == 0){
+                    fab.setVisibility(View.VISIBLE);
+
+                }else {
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (tabs.getSelectedTabPosition() == 0) {
-                    //   Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("New Subject");
+                //   Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("New Subject");
 
-                    EditText editText = new EditText(MainActivity.this);
-                    editText.setInputType(InputType.TYPE_CLASS_TEXT);
-                    builder.setView(editText);
+                EditText editText = new EditText(MainActivity.this);
+                editText.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(editText);
 
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                        }
-                    });
-                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                        }
-                    });
-                    builder.show();
-                }else {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                }
+                    }
+                });
+                builder.show();
             }
         });
+
     }
 }
