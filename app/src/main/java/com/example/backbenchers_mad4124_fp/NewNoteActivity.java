a3acc8f161c4f97f044902894077bcd4d9dc5c21
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-07-05 17:27:35 -0400

    new note activity

---
+++ app/src/main/java/com/example/backbenchers_mad4124_fp/NewNoteActivity.java
@@ -0,0 +1,26 @@
+package com.example.backbenchers_mad4124_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class NewNoteActivity extends AppCompatActivity {
+
+    private TextInputEditText title;
+    FloatingActionButton cameraFab;
+    FloatingActionButton audioFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_note);
+
+        title = findViewById(R.id.titleTextInputEditText);
+        cameraFab = findViewById(R.id.camerafab);
+        audioFab = findViewById(R.id.audiofab);
+    }
+}
--- app/src/main/java/com/example/backbenchers_mad4124_fp/NotesActivity.java
+++ app/src/main/java/com/example/backbenchers_mad4124_fp/NotesActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.backbenchers_mad4124_fp.R;
@@ -12,6 +14,7 @@ import com.example.backbenchers_mad4124_fp.adapters.NotesAdapter;
 import com.example.backbenchers_mad4124_fp.adapters.SubjectsAdapter;
 import com.example.backbenchers_mad4124_fp.models.Notes;
 import com.example.backbenchers_mad4124_fp.models.Subject;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Text;
 
@@ -23,11 +26,14 @@ public class NotesActivity extends AppCompatActivity implements Serializable {
     private RecyclerView notesrecyclerView;
     private ArrayList<Notes> notes;
     private NotesAdapter notesAdapter;
+    private FloatingActionButton notesFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        notesFab = findViewById(R.id.notesFab);
 
         notesrecyclerView = findViewById(R.id.notesRecyclerView);
 
@@ -39,6 +45,14 @@ public class NotesActivity extends AppCompatActivity implements Serializable {
                 StaggeredGridLayoutManager.VERTICAL);
         notesrecyclerView.setLayoutManager(mStaggeredGridLayoutManager);
         notesrecyclerView.setAdapter(notesAdapter);
+
+        notesFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotesActivity.this, NewNoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void populateNotes(){
