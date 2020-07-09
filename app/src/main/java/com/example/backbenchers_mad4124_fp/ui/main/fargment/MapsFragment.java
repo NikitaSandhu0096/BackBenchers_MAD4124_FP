df9a98daa40c816c4d120bb2e8f031b4887218af
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-07-09 16:39:45 -0400

    mapview

--- app/src/main/java/com/example/backbenchers_mad4124_fp/ui/main/fargment/MapsFragment.java
+++ app/src/main/java/com/example/backbenchers_mad4124_fp/ui/main/fargment/MapsFragment.java
@@ -10,8 +10,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.backbenchers_mad4124_fp.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment<onViewCreate> extends Fragment {
+public class MapsFragment<onViewCreate> extends Fragment implements OnMapReadyCallback {
+
+    MapView mapView;
+    GoogleMap map;
+
     public MapsFragment() {
         // Required empty public constructor
     }
@@ -28,6 +38,52 @@ public class MapsFragment<onViewCreate> extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
 
         super.onViewCreated(view, savedInstanceState);
+        mapView = view.findViewById(R.id.mapView);
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        map = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.5890, 79.6441)).title("Here"));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
